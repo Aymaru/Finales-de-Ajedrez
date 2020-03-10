251afe2fa815dce4e0d87efdb3bdec0d67f05c04
Unknown <aycastillo@ic-itcr.ac.cr>
2020-03-09 20:31:29 -0600

    Codigo comentado :)

--- finales_de_ajedrez.py
+++ finales_de_ajedrez.py
@@ -14,6 +14,8 @@ import copy
 ## columnas
 ##  a = 0  b = 1   c = 2   d = 3   e = 4   f = 5   g = 6   h = 7
 
+#############################################################################
+## 
 tablero = []
 
 ## Cada pieza se representa asi [ color, tipo, columna, fila ]
@@ -22,6 +24,10 @@ piezas_iniciales = [ ['N','R','a',8], ['B','C','c',8], ['B','D','f',8],
                      ['B','P','a',7], ['N','T','g',6], ['B','A','a',4],
                      ['N','P','g',4], ['N','A','g',3], ['B','T','b',2],
                      ['B','R','b',1]]
+
+####
+##  Funciones de conversion entre el formato de entrada y el utilizado
+####
 
 ## Genera un vector de 64 espacios vacios representados por 0
 ## Representa el tablero del juego
@@ -87,6 +93,35 @@ def convertir_pieza_tablero_a_pieza_real(pieza_tablero):
         
     return pieza_real
 
+####
+##  Funciones de que permiten ver las piezas y colocarlas en el tablero. Y otras funciones auxiliares
+####
+
+## Recibe la lista de piezas iniciales en el siguiente formato:
+##      [<c><p><col><fil>
+##  donde:
+##      <c> ::= {B|N}
+##      <p> ::= {R|D|T|A|C|P}
+##      <col> ::= {a|b|c|d|e|f|g|h}
+##      <fil> ::= {1|2|3|4|5|6|7|8}
+## Ejemplo: piezas_iniciales = [    ['N','R','a',8], ['B','C','c',8], ['B','D','f',8],
+##                                  ['B','P','a',7], ['N','T','g',6], ['B','A','a',4],
+##                                  ['N','P','g',4], ['N','A','g',3], ['B','T','b',2],
+##                                  ['B','R','b',1] ]
+## Devuelve el tablero con las piezas
+def colocar_piezas_iniciales_en_tablero(tablero,piezas_iniciales):
+    for i in range(0,len(piezas_iniciales)):
+        pieza_real = [ piezas_iniciales[i][0], piezas_iniciales[i][1] ]
+        posicion_real = [ piezas_iniciales[i][2], piezas_iniciales[i][3] ]
+        posicion = convertir_posicion_real_a_representacion(posicion_real)
+        pieza_tablero = convertir_pieza_real_a_pieza_tablero(pieza_real)
+        tablero = colocar_pieza_en_casilla(tablero,pieza_tablero,posicion)
+    return tablero
+
+## Verifica que una posicion sea valida.
+## Una posicion es valida si tiene el formato fila,columna. Ejm (2,1)
+## y los valores se encuentran entre 0 y 7
+## Devuelve True si la posicion es valida, False en caso contrario
 def validar_posicion(posicion):
     return not (posicion[0] < 0 or posicion[0] > 7 or posicion[1] < 0 or posicion[1] > 7)
 
@@ -94,19 +129,22 @@ def validar_posicion(posicion):
 ## Recibe una posicion (fila, columna)
 ## Devuelve un entero con el indice
 def calcular_posicion_tablero(posicion):
-    #print(posicion[0])
-    #print(posicion[1])
     i = 8*posicion[0] + posicion[1]
-    #print(i)
     return i
 
+## Recibe una posicion fila,columna Ejm.(2,1)
+## Devuelve el valor en esa posicion del tablero
 def obtener_pieza_de_casilla(tablero,posicion):
     return tablero[calcular_posicion_tablero(posicion)]
 
+## Recibe el valor de una pieza (1,2,3,4,5,6) o negativos y una posicion fila,columna Ejm. (2,1)
+## Devuelve el tablero con la pieza en esa casilla
 def colocar_pieza_en_casilla(tablero,pieza,posicion):
     tablero[calcular_posicion_tablero(posicion)] = pieza
     return tablero
 
+## imprime en consola el tablero con las piezas.
+## metodo momentaneo, mientras realizamos la interfaz.
 def imprimir_tablero():
     global tablero
     etiqueta_de_fila = ['8','7','6','5','4','3','2','1']
@@ -134,43 +172,85 @@ def imprimir_tablero():
     print(etiqueta_de_columna)
     return
 
-def colocar_piezas_iniciales_en_tablero():
-    global tablero, piezas_iniciales
-    tablero_con_piezas = tablero
-    for i in range(0,len(piezas_iniciales)):
-        pieza_real = [ piezas_iniciales[i][0], piezas_iniciales[i][1] ]
-        posicion_real = [ piezas_iniciales[i][2], piezas_iniciales[i][3] ]
-        posicion = convertir_posicion_real_a_representacion(posicion_real)
-        pieza_tablero = convertir_pieza_real_a_pieza_tablero(pieza_real)
-        colocar_pieza_en_casilla(tablero_con_piezas,pieza_tablero,posicion)
-    tablero = tablero_con_piezas
-    return
-
-
+## Recibe una posicion fila,columna de una pieza. Ejm (2,1)
+## Devuelve 'B' si la pieza es blanca o 'N' si la pieza es negra
+## En este caso las blancas se representan con numeros positivos del 1 al 6
+## y las negras se representan con numeros negativos del 1 al 6
 def obtener_color_de_pieza(tablero,posicion):
     pieza = obtener_pieza_de_casilla(tablero,posicion)
     if pieza > 0:
         return 'B'
-    else:
-        return 'N'
+    return 'N'
     
+####
+##  Funcionalidades que describen el juego, mover una pieza del tablero, generar los posibles movimientos en un turno, determinar jaque y jaque mate
+####
 
-## Funcionalidades que describen el juego
-def mover_pieza(tablero,posicion_inicial,posicion_final,pieza_de_coronamiento):
+## Realiza un movimiento legal de una pieza
+## Recibe un movimiento legal y la una pieza en caso de coronamiento
+## Devuelve el tablero con el movimiento realizado
+## Un movimiento legal, son todos los posibles movimientos que la posicion inicial contenga una pieza del color del jugador,
+## es decir, si el jugador esta jugando con las blancas, la pieza debe ser un numero positivo entre 1 y 6.
+## Y si el jugador esta jugando con las negras, la pieza debe ser un numero negativo entre 1 y 6
+## Y recibe una pieza de coronamiento. Generalmente es una reina, -5 para las negras y 5 para las blancas  
+def mover_pieza(tablero,movimiento,pieza_de_coronamiento):
+    posicion_inicial = movimiento[0]
+    posicion_final = movimiento[1]
     pieza_a_mover = obtener_pieza_de_casilla(tablero,posicion_inicial)
     tablero = colocar_pieza_en_casilla(tablero,0,posicion_inicial)
     casilla_objetivo = obtener_pieza_de_casilla(tablero,posicion_final)
     if pieza_a_mover == 1 and posicion_final[0] == 0 and casilla_objetivo == 0:
-        tablero = colocar_pieza_en_casilla(tablero,6,posicion_final)
+        tablero = colocar_pieza_en_casilla(tablero,5,posicion_final)
     elif pieza_a_mover == -1 and posicion_final[0] == 6 and casilla_objetivo == 0:
-        tablero = colocar_pieza_en_casilla(tablero,-6,posicion_final)
+        tablero = colocar_pieza_en_casilla(tablero,-5,posicion_final)
     else:
         tablero = colocar_pieza_en_casilla(tablero,pieza_a_mover,posicion_final)
     return tablero
 
+## Funcion auxiliar, modifica fila,columna de una posicion a otra.
+## Recibe una posicion (2,1), y dos enteros para aumentar cada uno respectivamente
+## ejem avanzar_casilla((2,1),-1,+1) -> (1,2). El cual representa avanzar una casilla en la diagonal superior derecha
 def avanzar_casilla(posicion,avanzar_fila,avanzar_columna):
     return [posicion[0] + avanzar_fila, posicion[1] + avanzar_columna]
 
+## Funcion auxiliar que genera todos los posibles movimientos en una direccion.
+## Las posibles direcciones son (izquierda, derecha, arriba, abajo,
+## diagonal superior izquierda, diagonal superior derecha, diagonal inferior izquierda, diagonal inferior derecha).
+## Evalua casilla por casilla hasta que la posicion se salga del tablero, se encuentre una pieza enemiga o una pieza propia
+## Si la casilla esta vacia, agrega el posible movimiento y avanza
+## Si se encuentra una pieza propia o una posicion fuera del tablero y termina
+## Si se encuentra una pieza enemiga, agrega el posible movimiento y termina
+## Recibe una posicion fila,columna Ejm (2,1). y dos enteros para utilizar la funcion 'avanzar_casilla(posicion,avanzar_fila,avanzar_columna)'
+def posibles_movimientos_en_una_direccion(tablero,posicion,avanzar_fila,avanzar_columna):
+    posibles_movimientos = []
+
+    color_de_pieza = obtener_color_de_pieza(tablero,posicion)
+    fila = posicion[0] + avanzar_fila
+    columna = posicion[1] + avanzar_columna
+    while True:
+        if not validar_posicion([fila,columna]):
+            break
+        casilla_objetivo = obtener_pieza_de_casilla(tablero,[fila,columna])
+        
+        if casilla_objetivo != 0:
+                color_casilla_objetivo = obtener_color_de_pieza(tablero,[fila,columna])
+                if color_casilla_objetivo != color_de_pieza:
+                    posibles_movimientos.append([posicion,[fila,columna]])
+                    break
+                else:
+                    break
+        else:
+            posibles_movimientos.append([posicion,[fila,columna]])
+            fila += avanzar_fila
+            columna += avanzar_columna
+    return posibles_movimientos
+    
+## Funcion que obtiene los posibles movimientos de un peon
+## El peon puede moverse un campo para adelante a la vez.
+## Si encuentra enemigos en las diagonales superiores o inferiores, dependiendo el color, puede realizar una captura
+## Si el peon se encuentra en su posicion inicial de la partida, puede hacer una salida larga y avanzar dos espacios en vez de uno
+## Recibe una posicion (2,1)
+## Devuelve los posibles movimientos para el peon en esa posicion.
 def posibles_movimientos_de_peon(tablero,posicion):
     posibles_movimientos = []
     
@@ -207,44 +287,35 @@ def posibles_movimientos_de_peon(tablero,posicion):
     return posibles_movimientos
             
     
-def posibles_movimientos_en_una_direccion(tablero,posicion,avanzar_fila,avanzar_columna):
-    posibles_movimientos = []
-
-    color_de_pieza = obtener_color_de_pieza(tablero,posicion)
-    fila = posicion[0] + avanzar_fila
-    columna = posicion[1] + avanzar_columna
-    while True:
-        if not validar_posicion([fila,columna]):
-            break
-        casilla_objetivo = obtener_pieza_de_casilla(tablero,[fila,columna])
-        
-        if casilla_objetivo != 0:
-                color_casilla_objetivo = obtener_color_de_pieza(tablero,[fila,columna])
-                if color_casilla_objetivo != color_de_pieza:
-                    posibles_movimientos.append([posicion,[fila,columna]])
-                    break
-                else:
-                    break
-        else:
-            posibles_movimientos.append([posicion,[fila,columna]])
-            fila += avanzar_fila
-            columna += avanzar_columna
-    return posibles_movimientos
-    
-
+## Funcion que obtiene los posibles movimientos que puede realizar un alfil
+## El alfil puede recorrer todas las diagonales.
+## Recibe una posicion (2,1)
+## Devuelve los posibles movimientos para el alfil en esa posicion
 def posibles_movimientos_de_alfil(tablero,posicion):
     posibles_movimientos = posibles_movimientos_en_una_direccion(tablero,posicion,-1,-1) + posibles_movimientos_en_una_direccion(tablero,posicion,-1,1) + posibles_movimientos_en_una_direccion(tablero,posicion,1,-1) + posibles_movimientos_en_una_direccion(tablero,posicion,1,1)
     return posibles_movimientos
-    
+
+## Funcion que obtiene los posibles movimientos que puede realizar una torre
+## La torre puede recorrer todos los espacios posibles hacia arriba, abajo, derecha e izquierda
+## Recibe una posicion (2,1)
+## Devuelve los posibles movimientos para la torre en esa posicion   
 def posibles_movimientos_de_torre(tablero,posicion):
     posibles_movimientos = posibles_movimientos_en_una_direccion(tablero,posicion,-1,0) + posibles_movimientos_en_una_direccion(tablero,posicion,1,0) + posibles_movimientos_en_una_direccion(tablero,posicion,0,-1) + posibles_movimientos_en_una_direccion(tablero,posicion,0,1)
     return posibles_movimientos
 
+## Funcion que obtiene los posibles movimientos que puede realizar una Dama
+## la dama puede recorrer todas las direcciones, que son una combinacion de los posibles movimientos de un alfil y de una torre
+## Recibe una posicion (2,1)
+## Devuelve los posibles movimientos para la dama en esa posicion  
 def posibles_movimientos_de_dama(tablero,posicion):
     posibles_movimientos = posibles_movimientos_de_alfil(tablero,posicion) + posibles_movimientos_de_torre(tablero,posicion)
     return posibles_movimientos
 
-
+## Funcion que obtiene los posibles movimientos que puede realizar el caballo
+## El caballo puede avanzar en una L a cualquier direccion.
+## Recibe una posicion (2,1)
+## Devuelve los posibles movimientos para el caballo en esa posicion 
+ 
 def posibles_movimientos_de_caballo(tablero,posicion):
     posibles_movimientos = []
     
@@ -282,7 +353,11 @@ def posibles_movimientos_de_caballo(tablero,posicion):
                     posibles_movimientos.append([posicion,[fila,columna]])
                     
     return posibles_movimientos
-            
+
+## Funcion que obtiene los posibles movimientos que puede realizar el rey
+## El rey puede avanzar unicamente un espacio a cualquier direccion
+## Recibe una posicion (2,1)
+## Devuelve los posibles movimientos para el rey en esa posicion             
 def posibles_movimientos_de_rey(tablero,posicion):
     posibles_movimientos = []
     
@@ -306,7 +381,7 @@ def posibles_movimientos_de_rey(tablero,posicion):
     return posibles_movimientos
 
 
-
+## Genera todos los posibles movimientos, de todas las piezas en un tablero
 def generar_posibles_movimientos(tablero):
     posibles_movimientos = []
     
@@ -333,10 +408,10 @@ def generar_posibles_movimientos(tablero):
 
 
 
-## Verificar jaque
-## verificar mov legal
-## verificar movimientos legales
-
+## Verificar si un jugador se encuentra en jaque
+## Recibe los posibles movimientos del tablero 'generar_posibles_movimientos(tablero)'
+## y el turno del jugador 'B' o 'N', blanco o negro respectivamente
+## Devuelve True si se encuentra en jaque o False en caso contrario
 def hay_jaque(tablero,posibles_movimientos,turno):
 
     for movimiento in posibles_movimientos:
@@ -349,6 +424,13 @@ def hay_jaque(tablero,posibles_movimientos,turno):
 
     return False
 
+## Verifica si un movimiento es un movimiento legal
+## Un movimiento es legal, si se mueve una pieza del color que corresponda, si juegan las blancas solo los movimientos que toquen piezas blancas son legales.
+## Ademas, no se puede quedar en estado de jaque despues de realizar el movimiento.
+## Recibe un movimiento ([ posicion_inicial, [posicion_final] ])
+## Las posiciones son pares fila,columna (2,1)
+## y el turno del jugador 'B' o 'N', blanco o negro respectivamente
+## Devuelve True si se encuentra en jaque o False en caso contrario
 def es_movimiento_legal(tablero,movimiento,turno):
 
     if turno == 'B':
@@ -359,15 +441,18 @@ def es_movimiento_legal(tablero,movimiento,turno):
     color_de_pieza = obtener_color_de_pieza(tablero,movimiento[0])
     if color_de_pieza == turno:
         tablero_despues_de_movimiento = copy.deepcopy(tablero)
-        tablero_despues_de_movimiento = mover_pieza(tablero_despues_de_movimiento,movimiento[0],movimiento[1],pieza_de_coronamiento)
+        tablero_despues_de_movimiento = mover_pieza(tablero_despues_de_movimiento,movimiento,pieza_de_coronamiento)
         posibles_movimientos = generar_posibles_movimientos(tablero_despues_de_movimiento)
         if hay_jaque(tablero_despues_de_movimiento,posibles_movimientos,turno):
             return False
         else:
             return True
-    else:
-        return False
 
+    return False
+
+## Funcion que obtiene todos los movimientos legales para un jugador 'B' o 'N', blanco o negro respectivamente
+## Recibe todos los movimientos posibles y el turno del jugador
+## Devuelve la lista de movimientos legales
 def obtener_movimientos_legales(tablero,posibles_movimientos,turno):
     movimientos_legales = []
 
@@ -376,6 +461,7 @@ def obtener_movimientos_legales(tablero,posibles_movimientos,turno):
             movimientos_legales.append(movimiento)
     return movimientos_legales
 
+## Revisa si hay jaque mate
 def hay_jaque_mate(tablero,posibles_movimientos,turno):
     movimientos_legales = obtener_movimientos_legales(tablero,posibles_movimientos,turno)
     if len(movimientos_legales) == 0:
